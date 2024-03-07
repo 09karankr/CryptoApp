00576f995a15120b43bde0d3fe2c0247cdb80d1e
Karan Kumar <officialkaran39@gmail.com>
2024-03-07 20:53:46 +0530

    completed the ui layout

--- app/src/main/java/com/example/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/example/cryptoapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.cryptoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.PopupMenu
+import androidx.navigation.fragment.findNavController
+import com.example.cryptoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
+        val navController = navHostFragment!!.findNavController()
+
+        val popupMenu = PopupMenu(this, binding.root)
+        popupMenu.inflate(R.menu.bottom_nav_menu)
+        binding.bottomBar.setupWithNavController(popupMenu.menu,navController)
+
     }
 }
