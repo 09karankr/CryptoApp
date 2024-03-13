72830ee31a0f0deed3eea3e0c104e9cea6d2ecbf
Karan Kumar <officialkaran39@gmail.com>
2024-03-13 14:04:13 +0530

    Completed the top gain loss fragment and completed the adapter

--- app/src/main/java/com/example/cryptoapp/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/cryptoapp/fragment/HomeFragment.kt
@@ -6,15 +6,20 @@ import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.lifecycle.LifecycleCoroutineScope
 import androidx.lifecycle.lifecycleScope
+import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.manager.Lifecycle
 import com.example.cryptoapp.R
+import com.example.cryptoapp.adapter.TopLossGainPagerAdapter
 import com.example.cryptoapp.adapter.TopMarketAdapter
 import com.example.cryptoapp.apis.ApiInterface
 import com.example.cryptoapp.apis.ApiUtilities
 import com.example.cryptoapp.databinding.FragmentHomeBinding
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -30,7 +35,37 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(layoutInflater)
         getTopCurrencyList()
+        setTabLayout()
         return binding.root
+    }
+
+    private fun setTabLayout() {
+        val adapter = TopLossGainPagerAdapter(this)
+        binding.contentViewPager.adapter = adapter
+        binding.contentViewPager.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if (position == 0) {
+                    binding.topGainIndicator.visibility = VISIBLE
+                    binding.topLoseIndicator.visibility = GONE
+
+                } else {
+                    binding.topGainIndicator.visibility = GONE
+                    binding.topLoseIndicator.visibility = VISIBLE
+                }
+            }
+        })
+
+        TabLayoutMediator(binding.tabLayout, binding.contentViewPager) {tab, position ->
+            var title = if (position == 0){
+                "Top Gainers"
+        }else {
+            "Top Losers"
+        }
+        tab.text = title
+    }.attach()
+
     }
 
     private fun getTopCurrencyList(){
